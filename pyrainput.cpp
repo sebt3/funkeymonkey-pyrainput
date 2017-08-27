f7408fec10b05eb3c387a386eefae16215e0f6a2
Sébastien Huss <sebastien.huss@gmail.com>
2017-08-27 16:53:25 +0200

    improvements in packaging and mapping

--- pyrainput.cpp
+++ pyrainput.cpp
@@ -24,6 +24,7 @@ template<int FIRST_KEY, int LAST_KEY> class KeyBehaviors {
 public:
 	void passthrough(unsigned int code);
 	void map(unsigned int code, unsigned int result);
+	void gpmap(unsigned int code, unsigned int gamepad);
 	void altmap(unsigned int code, bool* flag, unsigned int regular, unsigned int alternative);
 	void complex(unsigned int code, std::function<void(int)> function);
 	void handle(unsigned int code, int value);
@@ -31,7 +32,7 @@ public:
 private:
 	struct KeyBehavior {
 		KeyBehavior() : type(PASSTHROUGH), mapping(0), function() {}
-		enum Type  { PASSTHROUGH, MAPPED, ALTMAPPED, COMPLEX };
+		enum Type  { PASSTHROUGH, MAPPED, ALTMAPPED, COMPLEX, GPMAPPED };
 		Type type;
 		int mapping;
 		int alternative;
@@ -119,14 +120,15 @@ void handleNubClick(Settings::NubClickMode mode, int value, Mouse* mouse, Uinput
 // Mouse movement/scroll thread handler
 void handleMouse(Mouse* mouse, Settings* settings, bool* stop);
 
-KeyBehaviors<FIRST_KEY, LAST_KEY>* behaviors;
-UinputDevice* out;
 struct {
 	bool stop = false;
 	UinputDevice* gamepad = nullptr;
+	UinputDevice* keyboard = nullptr;
+	KeyBehaviors<FIRST_KEY, LAST_KEY>* behaviors = nullptr;
 	Mouse* mouse = nullptr;
 	std::thread mouseThread;
 	Settings settings;
+	bool FnPressed = false;
 } global;
 
 
@@ -136,24 +138,83 @@ void init(char const** argv, unsigned int argc) {
 		keycodes.push_back(i);
 	}
 	
-	out = new UinputDevice("/dev/uinput", BUS_USB, "FunKeyMonkey keyboard", 1, 1, 1, {
+	global.keyboard = new UinputDevice("/dev/uinput", BUS_USB, "FunKeyMonkey keyboard", 1, 1, 1, {
 		{ EV_KEY, keycodes }
 	});
 	
-	behaviors = new KeyBehaviors<FIRST_KEY, LAST_KEY>();
-	
-	// Example mappings
-	behaviors->map(KEY_K, KEY_L);
-	behaviors->complex(KEY_O, [](int value) {
-		out->send(EV_KEY, KEY_I, value);
-		out->send(EV_KEY, KEY_O, value);
+	global.behaviors = new KeyBehaviors<FIRST_KEY, LAST_KEY>();
+	global.behaviors->gpmap(KEY_UP,		BTN_DPAD_UP);
+	global.behaviors->gpmap(KEY_DOWN,	BTN_DPAD_DOWN);
+	global.behaviors->gpmap(KEY_LEFT,	BTN_DPAD_LEFT);
+	global.behaviors->gpmap(KEY_RIGHT,	BTN_DPAD_RIGHT);
+	global.behaviors->gpmap(KEY_LEFTALT,	BTN_START);
+	global.behaviors->gpmap(KEY_LEFTCTRL,	BTN_SELECT);
+	global.behaviors->gpmap(KEY_HOME,	BTN_A);
+	global.behaviors->gpmap(KEY_END,	BTN_B);
+	global.behaviors->gpmap(KEY_PAGEDOWN,	BTN_X);
+	global.behaviors->gpmap(KEY_PAGEUP,	BTN_Y);
+	global.behaviors->gpmap(KEY_RIGHTSHIFT,	BTN_TL);
+	global.behaviors->gpmap(KEY_RIGHTMETA,	BTN_TL2);
+	global.behaviors->gpmap(KEY_RIGHTCTRL,	BTN_TR);
+	global.behaviors->gpmap(KEY_RIGHTALT,	BTN_TR2);
+	global.behaviors->gpmap(KEY_INSERT,	BTN_C);  //(I)
+	global.behaviors->gpmap(KEY_DELETE,	BTN_Z);  //(II)
+	global.behaviors->complex(KEY_LEFTMETA, [](int value) {
+		global.FnPressed = (value==1);
 	});
+	//TODO: make the alt mapping configurable
+	global.behaviors->altmap(KEY_ESC,	&global.FnPressed, KEY_ESC,	KEY_SYSRQ);
+	//global.behaviors->altmap(KEY_PAUSE,	&global.FnPressed, KEY_PAUSE,	);
+	//global.behaviors->altmap(KEY_BRIGHTNESSUP,	&global.FnPressed, KEY_BRIGHTNESSUP,	KEY_BRIGHTNESSDOWN);
+	global.behaviors->altmap(KEY_F11,	&global.FnPressed, KEY_F11,	KEY_F12);
+	global.behaviors->altmap(KEY_1,		&global.FnPressed, KEY_1,	KEY_F1);
+	global.behaviors->altmap(KEY_2,		&global.FnPressed, KEY_2,	KEY_F2);
+	global.behaviors->altmap(KEY_3,		&global.FnPressed, KEY_3,	KEY_F3);
+	global.behaviors->altmap(KEY_4,		&global.FnPressed, KEY_4,	KEY_F4);
+	global.behaviors->altmap(KEY_5,		&global.FnPressed, KEY_5,	KEY_F5);
+	global.behaviors->altmap(KEY_6,		&global.FnPressed, KEY_6,	KEY_F6);
+	global.behaviors->altmap(KEY_7,		&global.FnPressed, KEY_7,	KEY_F7);
+	global.behaviors->altmap(KEY_8,		&global.FnPressed, KEY_8,	KEY_F8);
+	global.behaviors->altmap(KEY_9,		&global.FnPressed, KEY_9,	KEY_F9);
+	global.behaviors->altmap(KEY_0,		&global.FnPressed, KEY_0,	KEY_F10);
+	global.behaviors->altmap(KEY_TAB,	&global.FnPressed, KEY_TAB,	KEY_CAPSLOCK);
+	//global.behaviors->altmap(KEY_Q,		&global.FnPressed, KEY_Q,	);
+	//global.behaviors->altmap(KEY_W,		&global.FnPressed, KEY_W,	);
+	//global.behaviors->altmap(KEY_E,		&global.FnPressed, KEY_E,	KEY_EURO); //Not working
+	//global.behaviors->altmap(KEY_R,		&global.FnPressed, KEY_R,	);
+	//global.behaviors->altmap(KEY_T,		&global.FnPressed, KEY_T,	);
+	//global.behaviors->altmap(KEY_Y,		&global.FnPressed, KEY_Y,	);
+	//global.behaviors->altmap(KEY_U,		&global.FnPressed, KEY_U,	);
+	//global.behaviors->altmap(KEY_I,		&global.FnPressed, KEY_I,	);
+	//global.behaviors->altmap(KEY_O,		&global.FnPressed, KEY_O,	);
+	//global.behaviors->altmap(KEY_P,		&global.FnPressed, KEY_P,	);
+	//global.behaviors->altmap(KEY_APOSTROPHE,	&global.FnPressed, KEY_APOSTROPHE,	);
+	//global.behaviors->altmap(KEY_A,		&global.FnPressed, KEY_A,	);
+	//global.behaviors->altmap(KEY_S,		&global.FnPressed, KEY_S,	);
+	//global.behaviors->altmap(KEY_D,		&global.FnPressed, KEY_D,	);
+	//global.behaviors->altmap(KEY_F,		&global.FnPressed, KEY_F,	);
+	//global.behaviors->altmap(KEY_G,		&global.FnPressed, KEY_G,	);
+	//global.behaviors->altmap(KEY_H,		&global.FnPressed, KEY_H,	);
+	//global.behaviors->altmap(KEY_J,		&global.FnPressed, KEY_J,	);
+	//global.behaviors->altmap(KEY_K,		&global.FnPressed, KEY_K,	);
+	//global.behaviors->altmap(KEY_L,		&global.FnPressed, KEY_L,	);
+	global.behaviors->altmap(KEY_COMMA,	&global.FnPressed, KEY_COMMA,	KEY_SEMICOLON);
+	global.behaviors->altmap(KEY_DOT,	&global.FnPressed, KEY_DOT,	KEY_SLASH);
+	global.behaviors->altmap(KEY_Z,		&global.FnPressed, KEY_Z,	KEY_EQUAL);
+	global.behaviors->altmap(KEY_X,		&global.FnPressed, KEY_X,	KEY_MINUS);
+	global.behaviors->altmap(KEY_C,		&global.FnPressed, KEY_C,	KEY_LEFTBRACE);
+	global.behaviors->altmap(KEY_V,		&global.FnPressed, KEY_V,	KEY_RIGHTBRACE);
+	global.behaviors->altmap(KEY_B,		&global.FnPressed, KEY_B,	KEY_BACKSLASH);
+	global.behaviors->altmap(KEY_N,		&global.FnPressed, KEY_N,	KEY_GRAVE);
+	//global.behaviors->altmap(KEY_M,		&global.FnPressed, KEY_M,	);
+	global.behaviors->altmap(KEY_SPACE,	&global.FnPressed, KEY_SPACE,	KEY_COMPOSE);
 	
+
 	global.gamepad = new UinputDevice("/dev/uinput", BUS_USB, "Modal Gamepad", 1, 1, 1, {
 		{ EV_KEY, {
-			BTN_SOUTH, BTN_EAST, BTN_NORTH, BTN_WEST, 
+			BTN_A, BTN_B, BTN_NORTH, BTN_WEST, 
 			BTN_TL, BTN_TR, BTN_TL2, BTN_TR2,
-			BTN_SELECT, BTN_START, BTN_MODE,
+			BTN_SELECT, BTN_START, BTN_C, BTN_Z,
 			BTN_DPAD_UP, BTN_DPAD_DOWN, BTN_DPAD_LEFT, BTN_DPAD_RIGHT,
 			BTN_THUMBL, BTN_THUMBR
 		} },
@@ -177,6 +238,7 @@ void init(char const** argv, unsigned int argc) {
 void handle(input_event const& e) {
 	switch(e.type) {
 	case EV_ABS:
+		/*printf("EV_ABS: %d (%d,%d,%d,%d)\n", e.code, ABS_X,ABS_Y,ABS_RX,ABS_RY);*/
 		switch(e.code) {
 		case ABS_X:
 			handleNubAxis(global.settings.leftNubModeX, e.value, global.mouse, global.gamepad, global.settings);
@@ -194,17 +256,34 @@ void handle(input_event const& e) {
 		};
 		break;
 	case EV_KEY:
-		behaviors->handle(e.code, e.value);
-		out->send(EV_SYN, 0, 0);
 		switch(e.code) {
+		case BTN_LEFT: // mouse click
+		case BTN_RIGHT:
+		case BTN_MIDDLE:
+			global.mouse->device.send(EV_KEY, e.code, e.value);
+			global.mouse->device.send(EV_SYN, 0, 0);
+			break;
 		case BTN_THUMBL:
 			handleNubClick(global.settings.leftNubClickMode, e.value, global.mouse, global.gamepad, global.settings);
 			break;
 		case BTN_THUMBR:
 			handleNubClick(global.settings.rightNubClickMode, e.value, global.mouse, global.gamepad, global.settings);
 			break;
-		default: break;
+		default: 
+			global.behaviors->handle(e.code, e.value);
+			global.keyboard->send(EV_SYN, 0, 0);
+			break;
 		}
+		break;
+	case EV_REL:
+		/* 
+		 * One of the nub is in mouse mode
+		 * TODO: Should forward the event to the mouse
+		global.mouse->device.send(EV_REL, e.code, e.value);
+		global.mouse->signal.notify_all();
+		printf("REL: type=%d, code=%d, value=%d\n", e.type, e.code, e.value);
+		 */
+		break;
 	}
 }
 
@@ -215,11 +294,11 @@ void destroy() {
 	if(global.gamepad) {
 		delete global.gamepad;
 	}
-	if(out) {
-		delete out;
+	if(global.keyboard) {
+		delete global.keyboard;
 	}
-	if(behaviors) {
-		delete behaviors;
+	if(global.behaviors) {
+		delete global.behaviors;
 	}
 
 	global.mouseThread.join();
@@ -373,8 +452,7 @@ void handleNubAxis(Settings::NubAxisMode mode, int value, Mouse* mouse, UinputDe
 	}
 }
 
-void handleNubClick(Settings::NubClickMode mode, int value, Mouse* mouse, UinputDevice* gamepad, Settings const& settings)
-{
+void handleNubClick(Settings::NubClickMode mode, int value, Mouse* mouse, UinputDevice* gamepad, Settings const& settings) {
 	switch(mode) {
 	case Settings::MOUSE_LEFT: {
 		std::lock_guard<std::mutex> lk(mouse->mutex);
@@ -452,7 +530,7 @@ template<int FIRST_KEY, int LAST_KEY> void KeyBehaviors<FIRST_KEY, LAST_KEY>::ma
 
 template<int FIRST_KEY, int LAST_KEY> void KeyBehaviors<FIRST_KEY, LAST_KEY>::altmap(unsigned int code, bool* flag, unsigned int regular, unsigned int alternative) {
 	auto& b = behaviors.at(code - FIRST_KEY);
-	b.type = KeyBehavior::MAPPED;
+	b.type = KeyBehavior::ALTMAPPED;
 	b.mapping = regular;
 	b.alternative = alternative;
 	b.flag = flag;
@@ -464,20 +542,31 @@ template<int FIRST_KEY, int LAST_KEY> void KeyBehaviors<FIRST_KEY, LAST_KEY>::co
 	b.function = function;
 }
 
+template<int FIRST_KEY, int LAST_KEY> void KeyBehaviors<FIRST_KEY, LAST_KEY>::gpmap(unsigned int code, unsigned int gamepad) {
+	auto& b = behaviors.at(code - FIRST_KEY);
+	b.type = KeyBehavior::GPMAPPED;
+	b.alternative = gamepad;
+}
+
 template<int FIRST_KEY, int LAST_KEY> void KeyBehaviors<FIRST_KEY, LAST_KEY>::handle(unsigned int code, int value) {
+	if (code <FIRST_KEY || code >LAST_KEY) return;
 	KeyBehavior const& kb = behaviors.at(code - FIRST_KEY);
 	switch(kb.type) {
 	case KeyBehavior::PASSTHROUGH:
-		out->send(EV_KEY, code, value);
+		global.keyboard->send(EV_KEY, code, value);
 		break;
 	case KeyBehavior::MAPPED:
-		out->send(EV_KEY, kb.mapping, value);
+		global.keyboard->send(EV_KEY, kb.mapping, value);
 		break;
 	case KeyBehavior::ALTMAPPED:
-		out->send(EV_KEY, *kb.flag ? kb.alternative : kb.mapping, value);
+		global.keyboard->send(EV_KEY, *kb.flag ? kb.alternative : kb.mapping, value);
 		break; 
 	case KeyBehavior::COMPLEX:
 		kb.function(value);
 		break;
+	case KeyBehavior::GPMAPPED:
+		global.keyboard->send(EV_KEY, code, value);
+		global.gamepad->send(EV_KEY, kb.alternative, value);
+		global.gamepad->send(EV_SYN, 0, 0);
 	};
 }
